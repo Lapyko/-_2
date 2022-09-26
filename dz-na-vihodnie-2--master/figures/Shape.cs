f4e71eb1a2ec65497be7e9450bd7ead790191f09
Lapyko <113054648+Lapyko@users.noreply.github.com>
2022-09-26 18:07:03 +0300

    Add files via upload

---
+++ dz-na-vihodnie-2--master/figures/Shape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using figures;
+
+namespace figures
+{
+    internal class Shape
+    {
+        public string color;
+
+        public Shape (string color)
+        {
+            this.color = color;
+        }
+
+        public void Draw(Circle circle, Rectangle rectangle)
+        {
+            Console.WriteLine("нарисовать фигуру " + circle, "нарисовать фигуру " + rectangle);  
+        }
+
+        internal void Draw(Shape shape)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
